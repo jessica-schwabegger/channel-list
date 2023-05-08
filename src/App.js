f7824ab94355b05878c89b34ab2e28716a7ce8df
Jessica Schwabegger <jessicaschwabegger@Jessicas-MacBook-Pro.local>
2023-05-08 14:18:42 +0200

    Select upcoming days

--- src/App.js
+++ src/App.js
@@ -2,34 +2,81 @@ import './App.css';
 import moment from 'moment';
 import { useEffect, useState } from "react";
 import { EpisodeDescription } from './components/EpisodeDescription';
+import { Button } from './components/Button';
 
 function App() {
+  const todaysDate = new Date().toLocaleDateString("sv-SE");
   const [scheduleData, setScheduleData] = useState();
+  let [counter, setCounter] = useState(0);
+  let [dateToDisplay, setDateToDisplay] = useState(todaysDate);
+  const [disableNext, setDisableNext] = useState(false);
+  const [disablePrevious, setDisablePrevious] = useState(true);
+
+
+  const getDateToDisplay = () => {
+    let date = new Date();
+    date.setDate(date.getDate() + counter);
+    setDateToDisplay(date.toLocaleDateString("sv-SE"));
+  };
 
   useEffect(() => {
-    const getChannels = async () => {
-      const data = await fetch("https://api.sr.se/api/v2/scheduledepisodes?channelid=164&format=json");
+    getDateToDisplay();
+    limitToSevenDays(counter);
+    console.log(dateToDisplay);
+    const getEpisodes = async () => {
+      const data = await fetch(`https://api.sr.se/api/v2/scheduledepisodes?channelid=164&date=${dateToDisplay}&format=json`);
       const response = await data.json();
       setScheduleData(response.schedule);
     };
 
-    getChannels();
-  },[]);
+    getEpisodes();
+  },[counter, dateToDisplay]);
 
-  const formatTime = (date) => {
-    const formatted =  moment(date).format();
+  const formatTime = (fullDate) => {
+    const formatted =  moment(fullDate).format();
     const newDate = new Date(formatted);
 
     const options = {
       hour: "2-digit",
       minute: "2-digit",
     };
-    return newDate.toLocaleString("en-GB", options);
+    return newDate.toLocaleString("sv-SE", options);
+  };
+
+  const limitToSevenDays = (counter) => {
+    if (counter === 7) {
+      setDisableNext(true);
+    } else {
+      setDisableNext(false);
+    }
+
+    if (counter !== 0) {
+      setDisablePrevious(false);
+    } else {
+      setDisablePrevious(true);
+    }
   }
 
   return (
     <div className="App">
-      <h1>Tablå</h1>
+        <h1>Tablå</h1>
+      <div className="select-date-section">
+        <p>Datum: {dateToDisplay}</p>
+        <div>
+          <Button
+            disabled={disablePrevious}
+            className={disablePrevious ? "disabled-button" : null}
+            onClick={() => setCounter(counter - 1)}>
+              Föregående
+          </Button>
+          <Button
+            disabled={disableNext}
+            className={disableNext ? "disabled-button" : null}
+            onClick={() => setCounter(counter + 1)}>
+            Nästa
+          </Button>
+        </div>
+      </div>
       {scheduleData && (
         scheduleData.map((episode, index) => (
           <div className="episode-wrapper" key={index}>
